f0a055191891f81014ebbd6e069ee5b93b21b287
Jan Fajerski <jfajerski@suse.com>
2017-02-15 16:20:08 +0100

    qa: add skeleton deepsea suite
    Signed-off-by: Jan Fajerski <jfajerski@suse.com>

--- qa/tasks/deepsea.py
+++ qa/tasks/deepsea.py
@@ -2,14 +2,13 @@
 Task to deploy clusters with DeepSea
 '''
 import logging
-import yaml
-
-from cStringIO import StrinIO
 
 from teuthology.config import config as teuth_config
 from teuthology.exceptions import CommandFailedError
 from teuthology.repo_utils import fetch_repo
+from teuthology import misc
 from teuthology.task import Task
+from util import get_remote_for_role
 
 log = logging.getLogger(__name__)
 
@@ -21,27 +20,23 @@ class DeepSea(Task):
 
     def setup(self):
         super(DeepSea, self).setup()
-        self.find_repo()
+        try:
+            self.master = self.config['master']
+        except KeyError:
+            raise ConfigError('deepsea requires a master')
 
-    def find_repo(self):
-        """
-        Locate the repo we're using; cloning it from a remote repo if necessary
-        """
-        repo = self.config.get('repo', '.')
-        if repo.startswith(('http://', 'https://', 'git@', 'git://')):
-            repo_path = fetch_repo(
-                repo,
-                self.config.get('branch', 'master'),
-            )
-        else:
-            repo_path = os.path.abspath(os.path.expanduser(repo))
-        log.info("DeepSea repo is here: {}".format(repo_path))
-        self.repo_path = repo_path
+        self.cluster_name, type_, self.master_id = misc.split_role(self.master)
+
+        if type_ != 'master':
+            msg = 'master role ({0}) must be a master'.format(self.master)
+            raise ConfigError(msg)
+
+        self.master_remote = get_remote_for_role(self.ctx, self.master)
 
     def begin(self):
         super(DeepSea, self).begin()
 
     def end(self):
-        super(RBDMirror, self).end()
+        super(DeepSea, self).end()
 
-task = Deepsea
+task = DeepSea
