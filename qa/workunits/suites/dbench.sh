123d3b45a3545550bb2ee644380d722dddd296c4
Jan Fajerski <jfajerski@suse.com>
2017-04-24 08:40:10 +0200

    tests: workunits: install dbench package from OBS (openSUSE/SLES only)
    ceph-cm-ansible does not install workunit dependencies on openSUSE or SLES.
    This commit works around that by making the dbench workunit install the dbench
    package from OBS on these OSes before trying to run the dbench executable.
    
    Signed-off-by: Jan Fajerski <jfajerski@suse.com>

--- qa/workunits/suites/dbench.sh
+++ qa/workunits/suites/dbench.sh
@@ -2,5 +2,24 @@
 
 set -e
 
+#
+# SUSE-specific hack to install dbench from OBS (begin)
+#
+source /etc/os-release
+[ "x${ID}x" = "xslesx" ] && [ "x${VERSION_ID}x" = "x12.1x" ] && suse_version="42.1"
+[ "x${ID}x" = "xopensusex" ] && [ "x${VERSION_ID}x" = "x42.1x" ] && suse_version="42.1"
+[ "x${ID}x" = "xslesx" ] && [ "x${VERSION_ID}x" = "x12.2x" ] && suse_version="42.2"
+[ "x${ID}x" = "xopensusex" ] && [ "x${VERSION_ID}x" = "x42.2x" ] && suse_version="42.2"
+[ "x${ID}x" = "xslesx" ] && [ "x${VERSION_ID}x" = "x12.3x" ] && suse_version="42.3"
+[ "x${ID}x" = "xopensusex" ] && [ "x${VERSION_ID}x" = "x42.3x" ] && suse_version="42.3"
+if [ ! -z "$suse_version" ] ; then
+    sudo zypper --non-interactive addrepo http://download.opensuse.org/repositories/benchmark/openSUSE_Leap_${suse_version}/benchmark.repo
+    sudo zypper --gpg-auto-import-keys ref
+    sudo zypper --non-interactive install dbench
+fi
+#
+# SUSE-specific hack to install dbench from OBS (end)
+#
+
 dbench 1
 dbench 10
