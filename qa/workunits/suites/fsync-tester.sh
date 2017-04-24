02c2219a59a945df02a6559930e3e627e0263305
Jan Fajerski <jfajerski@suse.com>
2017-04-24 09:12:04 +0200

    tests: workunit: install lsof package from OBS (openSUSE/SLES only)
    ceph-cm-ansible does not install workunit dependencies on openSUSE or SLES.
    This commit works around that by making the fsync-tester workunit install the lsof
    package from OBS on these OSes before trying to run the lsof executable.
    
    Signed-off-by: Jan Fajerski <jfajerski@suse.com>

--- qa/workunits/suites/fsync-tester.sh
+++ qa/workunits/suites/fsync-tester.sh
@@ -1,5 +1,24 @@
 #!/bin/sh -x
 
+#
+# SUSE-specific hack to install lsof from OBS (begin)
+#
+source /etc/os-release
+[ "x${ID}x" = "xslesx" ] && [ "x${VERSION_ID}x" = "x12.1x" ] && suse_version="42.1"
+[ "x${ID}x" = "xopensusex" ] && [ "x${VERSION_ID}x" = "x42.1x" ] && suse_version="42.1"
+[ "x${ID}x" = "xslesx" ] && [ "x${VERSION_ID}x" = "x12.2x" ] && suse_version="42.2"
+[ "x${ID}x" = "xopensusex" ] && [ "x${VERSION_ID}x" = "x42.2x" ] && suse_version="42.2"
+[ "x${ID}x" = "xslesx" ] && [ "x${VERSION_ID}x" = "x12.3x" ] && suse_version="42.3"
+[ "x${ID}x" = "xopensusex" ] && [ "x${VERSION_ID}x" = "x42.3x" ] && suse_version="42.3"
+if [ ! -z "$suse_version" ] ; then
+    sudo zypper --non-interactive addrepo http://download.opensuse.org/repositories/utilities/openSUSE_Leap_${suse_version}/utilities.repo
+    sudo zypper --gpg-auto-import-keys ref
+    sudo zypper --non-interactive install lsof
+fi
+#
+# SUSE-specific hack to install lsof from OBS (end)
+#
+
 set -e
 
 wget http://download.ceph.com/qa/fsync-tester.c
