8a1775f9fd4ef96a1dcce663abadb7dd908e055a
Nathan Cutler <ncutler@suse.com>
2018-11-12 13:32:02 +0100

    qa: run rados --striper as root via sudo
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- qa/workunits/deepsea/wip/foo.sh
+++ qa/workunits/deepsea/wip/foo.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -x
 WORKUNITS_DIR=$CEPH_BASE/qa/workunits
-rados --striper
+sudo rados --striper
 RETURN_VALUE=$?
 set -e
 test "$RETURN_VALUE" != 0
-rados --striper 2>&1 | tee test-striper-output
+sudo rados --striper 2>&1 | tee test-striper-output
 grep "unrecognized command --striper" test-striper-output
